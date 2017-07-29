216e64bda9cc5d00003fd36576b87955996fca3e
misteraj <ajlongstreet@gmail.com>
2017-07-29 15:40:46 -0400

    css fixed for quote div

--- public/assets/js/quotes.js
+++ public/assets/js/quotes.js
@@ -5,27 +5,27 @@ const quotes = [
         citation: 'Percepliquis',
         year: '2012'},
 
-    {   quote:'“Use your sword properly, man! You are not some dandified gladiator. You are a legionary and a killer. Killers jab and stab!”',
+    {   quote:'Use your sword properly, man! You are not some dandified gladiator. You are a legionary and a killer. Killers jab and stab!',
         source: 'Marc Alan Edelheit',
         citation: 'The Tiger',
         year: '2015'},
 
-    {   quote: "“Practice isn't the thing you do once you're good. It's the thing you do that makes you good.”",
+    {   quote: "Practice isn't the thing you do once you're good. It's the thing you do that makes you good.",
         source: 'Malcolm Gladwell',
         citation: 'Outliers: The Story of Success',
         year: '2008'},
 
-    {   quote: '“To leave the world better than you found it, sometimes you have to pick up other people’s trash.”',
+    {   quote: 'To leave the world better than you found it, sometimes you have to pick up other people’s trash.',
         source: 'Bill Nye',
         citation: 'Undeniable: Evolution and the Science of Creation',
         year: '2014'},
 
-    {   quote: '“Maybe I’ll post a consumer review. “Brought product to surface of Mars. It stopped working. 0/10.”',
+    {   quote: 'Maybe I’ll post a consumer review. “Brought product to surface of Mars. It stopped working. 0/10.',
         source: 'Andy Weir',
         citation: 'The Martian',
         year: '2014'},
 
-    {   quote: "“Overconfidence is a strong ally. People are always surprised when you try to do things you can't.”",
+    {   quote: "Overconfidence is a strong ally. People are always surprised when you try to do things you can't.",
         source: 'Edward W. Robertson',
         citation: 'The White Tree',
         year: '2011'}
@@ -75,6 +75,7 @@ function printQuote(){
     print(formatMessage(getRandomQuote()))
 }
 
-setInterval(printQuote, 4000);
+printQuote();
+setInterval(printQuote, 6000);
 
 
