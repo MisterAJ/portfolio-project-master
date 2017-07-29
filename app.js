e291270a97f061f7f0ccbbfc245e1ac824bebf1f
misteraj <ajlongstreet@gmail.com>
2017-07-29 19:45:33 -0400

    Changed direction on Angular app

--- app.js
+++ app.js
@@ -13,7 +13,6 @@ const quoteController = require("./routes/quotes");
 app.use('/', mainController);
 app.use('/quotes/', quoteController);
 
-
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
