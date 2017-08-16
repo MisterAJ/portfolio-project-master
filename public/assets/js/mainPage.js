fea02c53ab6cf0d117965653793ff00c11ce04a2
misteraj <ajlongstreet@gmail.com>
2017-08-16 12:54:41 -0400

    Huge front end update

--- public/assets/js/mainPage.js
+++ public/assets/js/mainPage.js
@@ -2,12 +2,18 @@
 
 document.addEventListener('DOMContentLoaded', function () {
     Typed.new('.typedMessage', {
-        strings: ["Thanks for coming to visit me!^1000 Here are some projects I've been working on.^2000"
-            , "Down below are some links to my social media pages.^1000 Feel free to reach out to me in any way.",
-        "I would love to assist in developing your Python, JavaScript or Java projects.^1000\n Take a peak at my" +
-        " resumé below."],
+        strings: [
+          "Thanks for coming to visit me!^1000 I would love to assist in developing your Python, JavaScript or Java projects.^2000", 
+          "Below are links to my social media pages,^500 resumé,^500 and some cool projects I worked on recently.",
+          "Feel free to reach out to me in any way.^1000 I would love to create something awesome with you!"
+        ],
         showCursor: false,
         typeSpeed: 40
     });
 });
+
+/*strings: ["Thanks for coming to visit me!^1000 Here are some projects I've been working on.^2000"
+            , "Below are links to my social media pages.^1000 Feel free to reach out to me in any way.",
+        "I would love to assist in developing your Python, JavaScript or Java projects.^1000\n Take a peak at my" +
+        " resumé below."],*/
 
